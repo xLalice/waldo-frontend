1206c0a846b2be120c4046c9bdbc74c3d9568950
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-11-12 03:31:16 +0800

    Refactor Vite config to load environment variables and enhance server proxy settings

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-// vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  })
+}
