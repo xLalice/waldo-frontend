a556bbc361401972f94ebf6254528ffc3c29fcc4
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-11-12 03:31:27 +0800

    Remove unnecessary React import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import GameBoard from './components/GameBoard';
 import Leaderboard from './components/Leaderboard';
