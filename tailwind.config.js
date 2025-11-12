0d0dd4b58ab122802b767b423e9dfa020868a8a9
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-11-12 22:12:11 +0800

    Add custom animations and keyframes to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,31 @@ export default {
       fontFamily: {
         "custom" : ['Starborn'],
       },
+      animation: {
+        shake: 'shake 0.5s ease-in-out',
+        'pop-in': 'pop-in 0.3s ease-out forwards'
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '10%, 30%, 50%, 70%, 90%': { transform: 'translateX(-5px)' },
+          '20%, 40%, 60%, 80%': { transform: 'translateX(5px)' },
+        },
+        'pop-in': {
+          '0%': {
+            transform: 'translate(-50%, -50%) scale(0.5)',
+            opacity: '0',
+          },
+          '80%': {
+            transform: 'translate(-50%, -50%) scale(1.1)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'translate(-50%, -50%) scale(1)',
+            opacity: '1',
+          },
+        },
+      },
     },
   },
   plugins: [],
