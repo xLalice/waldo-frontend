e0108cede003a1444bd99b14d742a7d3f0981981
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-11-12 22:12:58 +0800

    Add status prop to TargetingBox for dynamic border styling

--- src/components/TargetingBox.jsx
+++ src/components/TargetingBox.jsx
@@ -1,18 +1,28 @@
-function TargetingBox({ position, scaleFactor }) {
-    const size = 100 * scaleFactor;
-    const offset = size / 2;
+function TargetingBox({ position, scaleFactor, status }) {
+  const size = 100 * scaleFactor;
+  const offset = size / 2;
 
-    return (
-        <div
-            className="absolute border-2 border-solid border-red pointer-events-none"
-            style={{
-                left: `${position.x * scaleFactor - offset}px`,
-                top: `${position.y * scaleFactor - offset}px`,
-                width: `${size}px`,
-                height: `${size}px`
-            }}
-        />
-    );
+  const baseClasses = "absolute border-4 border-solid pointer-events-none";
+
+  let statusClasses = "border-white";
+
+  if (status === "correct") {
+    statusClasses = "border-green-500";
+  } else if (status === "wrong") {
+    statusClasses = "border-red-500 animate-shake";
+  }
+
+  return (
+    <div
+      className={`${baseClasses} ${statusClasses}`}
+      style={{
+        left: `${position.x * scaleFactor - offset}px`,
+        top: `${position.y * scaleFactor - offset}px`,
+        width: `${size}px`,
+        height: `${size}px`,
+      }}
+    />
+  );
 }
 
 export default TargetingBox;
